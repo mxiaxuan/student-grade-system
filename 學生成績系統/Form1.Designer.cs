88e306d1454a26c27af87807a37e967e26f03e90
Mindy <st4u2636025252@gmail.com>
2024-07-19 15:06:26 +0800

    加入專案檔案。

---
+++ 學生成績系統/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace 學生成績系統
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl查詢結果顯示 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl成績總和 = new System.Windows.Forms.Label();
+            this.lbl成績平均 = new System.Windows.Forms.Label();
+            this.btn姓名排序 = new System.Windows.Forms.Button();
+            this.btn成績排序 = new System.Windows.Forms.Button();
+            this.btn列出所有學生資料 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn姓名搜尋 = new System.Windows.Forms.Button();
+            this.txt搜尋關鍵字 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(570, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "學生成績查詢";
+            // 
+            // lbl查詢結果顯示
+            // 
+            this.lbl查詢結果顯示.BackColor = System.Drawing.Color.Green;
+            this.lbl查詢結果顯示.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl查詢結果顯示.ForeColor = System.Drawing.Color.White;
+            this.lbl查詢結果顯示.Location = new System.Drawing.Point(30, 95);
+            this.lbl查詢結果顯示.Name = "lbl查詢結果顯示";
+            this.lbl查詢結果顯示.Size = new System.Drawing.Size(913, 538);
+            this.lbl查詢結果顯示.TabIndex = 1;
+            this.lbl查詢結果顯示.Text = "查詢結果顯示";
+            this.lbl查詢結果顯示.Click += new System.EventHandler(this.lbl查詢結果顯示_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(994, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "成績總和";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(994, 447);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 46);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "成績平均";
+            // 
+            // lbl成績總和
+            // 
+            this.lbl成績總和.BackColor = System.Drawing.Color.Green;
+            this.lbl成績總和.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl成績總和.ForeColor = System.Drawing.Color.White;
+            this.lbl成績總和.Location = new System.Drawing.Point(994, 373);
+            this.lbl成績總和.Name = "lbl成績總和";
+            this.lbl成績總和.Size = new System.Drawing.Size(201, 58);
+            this.lbl成績總和.TabIndex = 4;
+            this.lbl成績總和.Text = "0000";
+            // 
+            // lbl成績平均
+            // 
+            this.lbl成績平均.BackColor = System.Drawing.Color.Green;
+            this.lbl成績平均.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl成績平均.ForeColor = System.Drawing.Color.White;
+            this.lbl成績平均.Location = new System.Drawing.Point(994, 505);
+            this.lbl成績平均.Name = "lbl成績平均";
+            this.lbl成績平均.Size = new System.Drawing.Size(201, 58);
+            this.lbl成績平均.TabIndex = 5;
+            this.lbl成績平均.Text = "0000";
+            // 
+            // btn姓名排序
+            // 
+            this.btn姓名排序.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold);
+            this.btn姓名排序.Location = new System.Drawing.Point(38, 667);
+            this.btn姓名排序.Name = "btn姓名排序";
+            this.btn姓名排序.Size = new System.Drawing.Size(283, 67);
+            this.btn姓名排序.TabIndex = 6;
+            this.btn姓名排序.Text = "姓名排序";
+            this.btn姓名排序.UseVisualStyleBackColor = true;
+            this.btn姓名排序.Click += new System.EventHandler(this.btn姓名排序_Click);
+            // 
+            // btn成績排序
+            // 
+            this.btn成績排序.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold);
+            this.btn成績排序.Location = new System.Drawing.Point(363, 667);
+            this.btn成績排序.Name = "btn成績排序";
+            this.btn成績排序.Size = new System.Drawing.Size(283, 67);
+            this.btn成績排序.TabIndex = 7;
+            this.btn成績排序.Text = "成績排序";
+            this.btn成績排序.UseVisualStyleBackColor = true;
+            this.btn成績排序.Click += new System.EventHandler(this.btn成績排序_Click);
+            // 
+            // btn列出所有學生資料
+            // 
+            this.btn列出所有學生資料.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold);
+            this.btn列出所有學生資料.Location = new System.Drawing.Point(38, 760);
+            this.btn列出所有學生資料.Name = "btn列出所有學生資料";
+            this.btn列出所有學生資料.Size = new System.Drawing.Size(452, 67);
+            this.btn列出所有學生資料.TabIndex = 8;
+            this.btn列出所有學生資料.Text = "列出所有學生資料";
+            this.btn列出所有學生資料.UseVisualStyleBackColor = true;
+            this.btn列出所有學生資料.Click += new System.EventHandler(this.btn列出所有學生資料_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn姓名搜尋);
+            this.groupBox1.Controls.Add(this.txt搜尋關鍵字);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(739, 656);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(618, 184);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "搜尋功能";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btn姓名搜尋
+            // 
+            this.btn姓名搜尋.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold);
+            this.btn姓名搜尋.ForeColor = System.Drawing.Color.Black;
+            this.btn姓名搜尋.Location = new System.Drawing.Point(355, 44);
+            this.btn姓名搜尋.Name = "btn姓名搜尋";
+            this.btn姓名搜尋.Size = new System.Drawing.Size(239, 102);
+            this.btn姓名搜尋.TabIndex = 10;
+            this.btn姓名搜尋.Text = "姓名搜尋";
+            this.btn姓名搜尋.UseVisualStyleBackColor = true;
+            this.btn姓名搜尋.Click += new System.EventHandler(this.btn姓名搜尋_Click);
+            // 
+            // txt搜尋關鍵字
+            // 
+            this.txt搜尋關鍵字.Location = new System.Drawing.Point(39, 71);
+            this.txt搜尋關鍵字.Multiline = true;
+            this.txt搜尋關鍵字.Name = "txt搜尋關鍵字";
+            this.txt搜尋關鍵字.Size = new System.Drawing.Size(264, 54);
+            this.txt搜尋關鍵字.TabIndex = 0;
+            this.txt搜尋關鍵字.TextChanged += new System.EventHandler(this.txt搜尋關鍵字_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1420, 875);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn列出所有學生資料);
+            this.Controls.Add(this.btn成績排序);
+            this.Controls.Add(this.btn姓名排序);
+            this.Controls.Add(this.lbl成績平均);
+            this.Controls.Add(this.lbl成績總和);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl查詢結果顯示);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Array進階搜尋";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl查詢結果顯示;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl成績總和;
+        private System.Windows.Forms.Label lbl成績平均;
+        private System.Windows.Forms.Button btn姓名排序;
+        private System.Windows.Forms.Button btn成績排序;
+        private System.Windows.Forms.Button btn列出所有學生資料;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn姓名搜尋;
+        private System.Windows.Forms.TextBox txt搜尋關鍵字;
+    }
+}
+
---
+++ 學生成績系統/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 學生成績系統
+{
+    public partial class Form1 : Form
+    {
+        //Class類別變數,寫在外面才能整個from都有該陣列
+        string[] arrayStudentName;//學生姓名陣列
+        int[] arrayStidentScore;//學生成績陣列
+        string[] arrayTempStudentName;//學生暫存姓名陣列，需要暫存的原因為這些是傳址呼叫，會影響原有的資料，所以使用暫存
+        int[] arrayTempStidentScore;//學生暫存成績陣列
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            arrayStudentName = new string[5];//index索引對應關係，key-value對應，key鍵
+            arrayStudentName[0] = "王曉明";
+            arrayStudentName[1] = "丁大維";
+            arrayStudentName[2] = "柯憶純";
+            arrayStudentName[3] = "許雅瑄";
+            arrayStudentName[4] = "黃宇琳";
+
+            arrayStidentScore = new int[5];//index索引對應關係，value值
+            arrayStidentScore[0] = 50;
+            arrayStidentScore[1] = 80;
+            arrayStidentScore[2] = 60;
+            arrayStidentScore[3] = 100;
+            arrayStidentScore[4] = 70;
+
+            arrayTempStudentName = new string[5];
+            arrayTempStidentScore = new int[5];
+
+            lbl成績總和.Text=string.Format("{0}", 計算總分(arrayStidentScore));
+            lbl成績平均.Text= string.Format("{0}", 計算平均(arrayStidentScore));
+        }
+
+        int 計算總分(int[] myArrayScore)
+        {
+            int sum = 0;
+            foreach(int score in myArrayScore)
+            {
+                sum += score;
+            }
+
+            return sum;
+        }
+
+        Double 計算平均(int[] myArrayScore)
+        {
+            Double avege = 0.0;
+            foreach (Double avegei in myArrayScore)
+            {
+                avege = 計算總分(myArrayScore) / myArrayScore.Length   ;
+            }
+
+            return avege;
+        }
+
+        private void lbl查詢結果顯示_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn姓名排序_Click(object sender, EventArgs e)
+        {
+            Array.Copy(arrayStudentName,arrayTempStudentName,arrayStudentName.Length);
+            //把原始的學生姓名檔跟長度複製到暫存檔去，以免下面列出所有學生資料時也被排序到了
+            姓名排序(arrayTempStudentName);
+        }
+
+        void 姓名排序(string[] myArrayName)
+        {
+            System.Array.Sort(myArrayName);//符號0-9 a-z 中文由筆畫少到多
+
+            string strMsg = "";
+            
+            for(int index =0; index<myArrayName.Length ;index ++)
+            {
+                strMsg += string.Format("姓名：{0}\n", myArrayName[index]);
+            }
+            lbl查詢結果顯示.Text = strMsg;
+        }
+
+        void 成績排序(string[] myArrayName,int[] myArrayScore) 
+        {
+
+            Array.Sort(myArrayScore,myArrayName);//鍵值對應排序
+            //輸出成績與姓名，成績由高到低輸出
+
+            string strMsg = "";
+
+            for (int index = myArrayName.GetUpperBound(0); index >= 0; index--)
+            //GetUpperBound取出陣列最大的索引值
+            {
+                strMsg += string.Format("姓名：{0} 成績:{1}\n", myArrayName[index], myArrayScore[index]);
+                //{0},{1}不是陣列的[]，所以{0}為表參數列表中的第一个和第二个元素
+            }
+            lbl查詢結果顯示.Text = strMsg;
+        }
+
+        private void btn成績排序_Click(object sender, EventArgs e)
+        {
+            Array.Copy(arrayStudentName, arrayTempStudentName, arrayStudentName.Length);
+            //把原始的學生姓名檔跟長度複製到暫存檔去，以免下面列出所有學生資料時也被排序到了
+            Array.Copy(arrayStidentScore, arrayTempStidentScore, arrayStidentScore.Length);
+            成績排序(arrayTempStudentName, arrayTempStidentScore);
+        }
+
+        private void btn列出所有學生資料_Click(object sender, EventArgs e)
+        {
+            列出所有資料();
+        }
+        void 列出所有資料()
+        {
+            String strMsg = "";
+
+            int i = 0;
+            foreach(String s in arrayStudentName)
+            //約等於for迴圈，但可以取得所有元素 不能限制範圍
+            {
+                strMsg += $"索引值: {i} 姓名: {s} 成績:{arrayStidentScore[i]}\n";
+                i += 1;
+            }
+            lbl查詢結果顯示.Text = strMsg;
+        }
+
+        private void btn姓名搜尋_Click(object sender, EventArgs e)
+        {
+            if (txt搜尋關鍵字.TextLength > 0)
+            {
+                int index, 名次 = 0;
+                string strmsg = "搜尋結果\n-------------------\n";
+                string strserchkey = txt搜尋關鍵字.Text;
+                
+                Array.Copy(arrayStudentName, arrayTempStudentName, arrayStudentName.Length);
+                Array.Copy(arrayStidentScore, arrayTempStidentScore, arrayStidentScore.Length);
+
+                //成績排序(arrayTempStudentName, arrayTempStidentScore);
+                Array.Sort(arrayTempStidentScore, arrayTempStudentName);//鍵值對應排序
+
+                index = Array.IndexOf(arrayTempStudentName, strserchkey);
+                //Array.IndexOf()蒐集集合的內容
+                //在strserchkey中的textbox裡中蒐集學生姓名資料
+
+                if (index <= -1)
+                //找不到對應的姓名，則index將為-1
+                {
+                    strmsg += "查無此人";
+                }
+                else
+                {
+                    名次 = arrayTempStudentName.Length - index;
+                    //陣列長度減低分高分的索引
+                    //假设数组已经按照成绩从高到低排序，而且没有重复的姓名。在这种情况下，index 表示的是该姓名在排序后数组中的位置，而数组的长度减去该位置即可得到名次。
+                    strmsg += $"姓名:{arrayTempStudentName[index]}成績:{arrayTempStidentScore[index]}第{名次}名";
+
+                }
+                lbl查詢結果顯示.Text = strmsg;
+            }
+            else
+            {
+                MessageBox.Show("請輸入資料");
+            }
+        }
+
+        private void txt搜尋關鍵字_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
